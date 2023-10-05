1d038df6c0607b98f4d4d30802ae801fce43ca06
Manuel Teodoro <teoten@gmail.com>
2023-10-05 07:48:17 +0200

    added favicon for prod

--- R/app_ui.R
+++ R/app_ui.R
@@ -37,7 +37,7 @@ golem_add_external_resources <- function() {
     ##   tags$link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65", crossorigin="anonymous"),
   ##   tags$script(src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4", crossorigin="anonymous"),
 
-    favicon(),
+    favicon(ext = get_golem_config("favicon")),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "PersonalFinances"
