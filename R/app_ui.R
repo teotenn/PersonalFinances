90f7f73cf2b4b53250508b7ce00dffcd04697877
Manuel Teodoro <teoten@gmail.com>
2023-10-05 08:41:37 +0200

    Work around yml file

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,3 +1,5 @@
+conf <- get_golem_config("env")
+
 #' The application User-Interface
 #'
 #' @param request Internal parameter for `{shiny}`.
@@ -11,7 +13,6 @@ app_ui <- function(request) {
     # Your application UI logic
     fluidPage(
       h1("PersonalFinances", align = "center"),
-      h3(get_golem_config("msg")),
       br(),
       mod_savings_ui("savings_1"),
       mod_footer_ui("footer_1")
@@ -38,7 +39,7 @@ golem_add_external_resources <- function() {
     ##   tags$link(href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65", crossorigin="anonymous"),
   ##   tags$script(src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4", crossorigin="anonymous"),
 
-    favicon(ext = get_golem_config("favicon")),
+    favicon(ext = get_golem_config("favicon", config = conf)),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "PersonalFinances"
